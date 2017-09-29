5c394bc828d6eb7db1ee19d72b7e15fb68cb04c4
Ariful Islam <arifsofg@gmail.com>
2017-09-30 03:45:42 +0600

    First Data Souce Insert

---
+++ classes/DB.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ariful Islam
+ * Date: 30-Sep-17
+ * Time: 3:44 AM
+ */
+
+class DB
+{
+    public $link;
+    public $username;
+    public $host;
+    public $password;
+    public $database;
+}
