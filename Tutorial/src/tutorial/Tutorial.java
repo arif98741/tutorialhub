2bdd706d195c2cea189d485eba48442e99afba40
arif98741 <arif98741@gmail.com>
2017-11-17 04:36:05 +0600

    Printing 5 Array String Values and Print Them in Java

--- Tutorial/src/tutorial/Tutorial.java
+++ Tutorial/src/tutorial/Tutorial.java
@@ -6,15 +6,17 @@ public class Tutorial {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int x = 5;
-        int[] values = new int[x];
+        System.out.print("Enter the size of array: ");
+        int x = sc.nextInt();
+        String[] values = new String[x]; //array declaration
         for (int i = 0; i < x; i++) {//taking values from user for five times
-            values[i] = sc.nextInt();
+            values[i] = sc.next(); //putting value in array's index
         }
         for (int i = 0; i < x; i++) { //printing all values 
             System.out.print(values[i] + " ");
         }
         System.out.println("");
     }
-
+//program for taking array size for string
+//print array values
 }
